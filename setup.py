a5653ad8ed8c56e1e58887be126cbdb0464a3301
astropradyumna <pradyumna.sadhu@gmail.com>
2023-12-30 12:21:49 -0800

    trying setup and init

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pyexample',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://https://github.com/astropradyumna/tng_routines',
+    author='Pradyumna Sadhu',
+    author_email='pradyumna.sadhu@gmail.com',
+    license='BSD 2-clause',
+    packages=['pyexample'],
+    install_requires=['numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
